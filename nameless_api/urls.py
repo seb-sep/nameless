a83209abb6a67f29c39c13cc2855c25412cdc90d
seb-sep <59621473+seb-sep@users.noreply.github.com>
2023-03-10 11:22:14 -0500

    Routes for searching teachers, courses, getting teacher

--- nameless_api/urls.py
+++ nameless_api/urls.py
@@ -3,5 +3,7 @@ from . import views
 
 urlpatterns = [
     path("", views.getData),
-    path("search/teacher/<str:name>", views.)
+    path("search/teacher/<str:name>", views.searchTeacher),
+    path("search/course/<str:name>", views.searchCourse),
+    path("teacher/<email:pk>", views.getTeacher),
 ]
