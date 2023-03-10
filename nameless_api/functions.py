feb6f99b28eb02eff60dc781465a934df298a050
seb-sep <59621473+seb-sep@users.noreply.github.com>
2023-03-10 11:21:43 -0500

    Factored out regex tokenization and added searching for courses

--- nameless_api/functions.py
+++ nameless_api/functions.py
@@ -1,4 +1,4 @@
-from feedback_man.models import Teacher
+from feedback_man.models import Teacher, Course
 import functools
 import re
 
@@ -7,8 +7,19 @@ def search_teacher_name(name: str):
     Search for teachers matching the given name in the database.
     Returns a QuerySet of all teachers who fit the search.
     """
+    return Teacher.objects.filter(teacher_name__iregex=regex_token_search(name))
 
+def search_course_name(name: str):
+    """"
+    Search for courses matching the given name in the database.
+    Returns a QuerySet of all courses who fit the search.
+    """
+    return Course.objects.filter(course_name__iregex=regex_token_search(name))
+
+def regex_token_search(query: str):
+    """
+    Create a regular expression for searching the database by matching for zero or more characters between
+    each token in the query.
+    """
     #regex of all tokens in the passed name with zero or more characters in between/on the ends of each
-    regex_str = functools.reduce(lambda regex, token: regex + re.escape(token+"*+"), name.split(), r"*+")
-
-    return Teacher.objects.filter(teacher_name__iregex=regex_str)
+    return functools.reduce(lambda regex, token: regex + re.escape(token+"*+"), query.split(), r"*+")
