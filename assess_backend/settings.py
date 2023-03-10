65a90b34ac2189055e1b847da591e7c779f29664
seb-sep <59621473+seb-sep@users.noreply.github.com>
2023-03-09 23:14:07 -0500

    Rest framework settings

--- assess_backend/settings.py
+++ assess_backend/settings.py
@@ -41,9 +41,16 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "feedback_man"
+    "feedback_man",
+    "rest_framework"
 
 ]
+
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+    )
+}
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
