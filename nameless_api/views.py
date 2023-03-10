a3db07412dab447b29dc0eae1939d935289efd78
seb-sep <59621473+seb-sep@users.noreply.github.com>
2023-03-10 11:22:40 -0500

    Endpoints for getting teacher, course, teacher search

--- nameless_api/views.py
+++ nameless_api/views.py
@@ -1,7 +1,8 @@
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from feedback_man.models import Message
-from .serializers import MessageSerializer
+from feedback_man.models import Message, Teacher
+from .serializers import MessageSerializer, TeacherSerializer, CourseSerializer
+from . import functions
 
 @api_view(['GET'])
 def getData(request):
@@ -10,4 +11,41 @@ def getData(request):
 
 @api_view(['GET'])
 def searchTeacher(request, name):
-    teachers = Teachers.objects.filter()
+    """
+    Return a JSON response of all teachers in the database who match the given name.
+    """
+    teachers = functions.search_teacher_name(name)
+    serializer = TeacherSerializer(teachers, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def searchCourse(request, name):
+    """
+    Return a JSON response of all courses in the database who match the given course.
+    """
+    courses = functions.search_course_name(name)
+    serializer = CourseSerializer(courses, many=True)
+    return Response(serializer.data)
+
+@api_view(['GET'])
+def getTeacher(request, pk):
+    """
+    Return a JSON response of the teacher with the given email.
+    """
+
+    teacher = Teacher.objects.get(id=pk)
+    serializer = TeacherSerializer(teacher)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def sendMessage(request):
+    """
+    Send an anonymous message to the teacher from the student,
+    as specified by the message body.
+    """
+
+    serializer = MessageSerializer(data=request.data)
+
+    if serializer.is_valid():
+        #Save to the database and send the message contents to the teacher's email
+        serializer.save()
